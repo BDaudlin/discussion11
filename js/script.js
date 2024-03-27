f0ebe622d761e071b70e0ecb57ddffcf61cd21c0
Brennen Daudlin <123896143+BDaudlin@users.noreply.github.com>
2024-03-27 18:49:57 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,18 +4,56 @@ let pokemonList = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","C
 
 // Question 1
 
+window.onload = function() {
 
+    question1();
 
+    function question1() {
+        document.getElementById("host-name").innerHTML = "Brennen Daudlin";
+        document.getElementById("collaborator-name").innerHTML = "Rafael Khoury";
+        document.getElementById("collaborator2-name").innerHTML = "N/A";
+    };
+
+}
 
 // Question 2
 // You can also create their own CSS classes in style.css file and use that. 
+var button = document.querySelector("#instruction_button");
+button.addEventListener("click", hideInstructions);
+function hideInstructions() {
+    console.log(this);
 
+    let setOfQuestions = document.querySelector(".question");
 
+    if (this.innerHTML === "Hide Instructions") {
+        for (let question of setOfQuestions) {
+            question.style.display = "none";
+            this.innerHTML = "Show Instructions";
+        }
+    }
+
+    else {
+        for (let question of setOfQuestions) {
+            question.style.display = "block";
+            this.innerHTML = "Show Instructions";
+        }
+    }
+}
 
 
 // Question 3
 // Try change event first
 // There is another event which fires for every character. This is what we want to use.
+function question3(){
+    var firstNameInput = document.getElementById('first_name');
+    var lastNameInput = document.getElementById('last_name');
+
+    if(firstNameInput.value === 'John'){
+        lastNameInput.value = 'Doe';
+    }else{
+        lastNameInput.value = '';
+    }
+}
 
 
 
